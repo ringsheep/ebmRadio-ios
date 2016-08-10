701fc394c6c93be4d7ed91112331fcd3028a32ff
George Zinyakov <g.zinyakov@evotor.ru>
2016-08-10 18:34:42 +0300

    pods added, streamingkit

---
+++ ebmRadio-ios/Pods/Target Support Files/Pods-ebmRadio-iosTests/Pods-ebmRadio-iosTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ebmRadio_iosTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ebmRadio_iosTestsVersionString[];
+
---
+++ ebmRadio-ios/Pods/Target Support Files/Pods-ebmRadio-iosUITests/Pods-ebmRadio-iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ebmRadio_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ebmRadio_iosUITests
+@end
